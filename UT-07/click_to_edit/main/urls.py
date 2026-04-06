59b279c039ffa549d6e4f1e7865a0768984e3910
Your Name <you@example.com>
2026-04-06 22:13:01 +0200

    draft

--- UT-07/click_to_edit/main/urls.py
+++ UT-07/click_to_edit/main/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/contact/1/edit', views.edit_name, name='edit_name'),
-    path('save/', views.save_name, name='save_name'),
+    path('contact/1/display/', views.display_name, name='display_name'),
+    path('contact/1/edit/', views.edit_name, name='edit_name'),
+    path('contact/1/save/', views.save_name, name='save_name'),
 ]
--- UT-07/click_to_edit/main/views.py
+++ UT-07/click_to_edit/main/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import get_user, update_user
 
 # Simulación de "base de datos"
 CURRENT_NAME = "Juan Pérez"
 
 def index(request):
-    return render(request, "index.html", {"name": CURRENT_NAME})
+    user = get_user()
+    return render(request, "index.html", {"user": user})
 
+def display_name(request):
+    user = get_user()
+    return render(request, "partials/name_display.html", {"user": user})
 
 def edit_name(request):
-    return render(request, "partials/edit_form.html", {"name": CURRENT_NAME})
+    user = get_user()
+    return render(request, "partials/edit_form.html", {"user": user})
 
 
 def save_name(request):
-    global CURRENT_NAME
-    CURRENT_NAME = request.POST.get("name", CURRENT_NAME)
-    return render(request, "partials/name_display.html", {"name": CURRENT_NAME})
+    user = update_user(
+        request.POST.get("firstName"),
+        request.POST.get("lastName"),
+        request.POST.get("email"),
+    )
+    return render(request, "partials/name_display.html", {"user": user})
